82693ab18eb9b7e6f35d5520e9964c0cc726dbd8
Otso Björklund <otso.bjorklund@gmail.com>
2019-03-06 17:55:58 +0200

    Issue #30: Add interface for pitched notation elements

---
+++ src/main/java/org/wmn4j/notation/elements/Pitched.java
@@ -0,0 +1,17 @@
+/*
+ * Distributed under the MIT license (see LICENSE.txt or https://opensource.org/licenses/MIT).
+ */
+package org.wmn4j.notation.elements;
+
+/**
+ * Interface for notation elements that have pitch.
+ */
+public interface Pitched {
+
+	/**
+	 * Returns the pitch of the notation element.
+	 *
+	 * @return the pitch of the notation element
+	 */
+	Pitch getPitch();
+}
