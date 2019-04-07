379aa1d614198b064322ce55d75df9ec6101af1d
MWargelin <matias.wargelin@gmail.com>
2019-04-07 20:54:52 +0300

    Issue #12: Update path to schema

--- src/main/java/org/wmn4j/io/musicxml/MusicXmlReader.java
+++ src/main/java/org/wmn4j/io/musicxml/MusicXmlReader.java
@@ -16,7 +16,7 @@ public interface MusicXmlReader extends ScoreReader {
 	/**
 	 * The path to the local MusicXML schema.
 	 */
-	Path SCHEMA = Paths.get("src/wmnlibio/musicxml/musicxml.xsd");
+	Path SCHEMA = Paths.get("src/main/java/org/wmn4j/io/musicxml/schema/musicxml.xsd");
 
 	/**
 	 * Returns a reader object with the given configuration.
