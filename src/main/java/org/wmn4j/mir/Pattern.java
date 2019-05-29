558d078fbc347d31555f64655037c05fb97c1e13
Otso Björklund <otso.bjorklund@gmail.com>
2019-05-29 19:53:27 +0300

    Issue #5: Remove use of <code> tags from Pattern documentation

--- src/main/java/org/wmn4j/mir/Pattern.java
+++ src/main/java/org/wmn4j/mir/Pattern.java
@@ -23,62 +23,62 @@ public interface Pattern {
 	boolean isMonophonic();
 
 	/**
-	 * Returns true if this <code>Pattern</code> contains the same pitches in the
+	 * Returns true if this pattern contains the same pitches in the
 	 * same order as other, otherwise returns false. The pitches must be spelled the
 	 * same way for the patterns to be considered equal in pitch. Rhythm is ignored.
 	 *
-	 * @param other the <code>Pattern</code> against which this is compared for
+	 * @param other the pattern against which this is compared for
 	 *              pitch equality.
-	 * @return true if this <code>Pattern</code> contains the same pitches in the
+	 * @return true if this pattern contains the same pitches in the
 	 * same order as other, otherwise returns false
 	 */
 	boolean equalsInPitch(Pattern other);
 
 	/**
-	 * Returns true if this <code>Pattern</code> contains the enharmonically same
+	 * Returns true if this pattern contains the enharmonically same
 	 * pitches in the same order as other, otherwise returns false. The pitch
 	 * spellings are not considered, but they are compared using enharmonic
 	 * equality. Rhythm is ignored.
 	 *
-	 * @param other the <code>Pattern</code> against which this is compared for
+	 * @param other the pattern against which this is compared for
 	 *              enharmonic pitch equality.
-	 * @return true if this <code>Pattern</code> contains the enharmonically same
+	 * @return true if this pattern contains the enharmonically same
 	 * pitches in the same order as other, otherwise returns false
 	 */
 	boolean equalsEnharmonicallyInPitch(Pattern other);
 
 	/**
-	 * Returns true if this <code>Pattern</code> can be transposed so that its
+	 * Returns true if this pattern can be transposed so that its
 	 * pitches are enharmonically equal to those of other, otherwise returns false.
 	 *
-	 * @param other the <code>Pattern</code> against which this is compared for
+	 * @param other the pattern against which this is compared for
 	 *              transposed enharmonic pitch equality.
-	 * @return true if this <code>Pattern</code> can be transposed so that its
+	 * @return true if this pattern can be transposed so that its
 	 * pitches are enharmonically equal to those of other, otherwise returns
 	 * false
 	 */
 	boolean equalsInTransposedPitch(Pattern other);
 
 	/**
-	 * Returns true if this <code>Pattern</code> has the same rhythm as other. Both
+	 * Returns true if this pattern has the same rhythm as other. Both
 	 * the onsets and durations must match for rhythms to be considered equal.
 	 * Pitches are ignored.
 	 *
-	 * @param other the <code>Pattern</code> against which this is compared for
+	 * @param other the pattern against which this is compared for
 	 *              rhythm equality.
-	 * @return true if this <code>Pattern</code> has the same rhythm as other,
+	 * @return true if this pattern has the same rhythm as other,
 	 * otherwise returns false
 	 */
 	boolean equalsInRhythm(Pattern other);
 
 	/**
-	 * Returns true if the onset times of notes in this <code>Pattern</code> match
+	 * Returns true if the onset times of notes in this pattern match
 	 * the onset times of notes in other, otherwise returns false. Pitches are
 	 * ignored.
 	 *
-	 * @param other the <code>Pattern</code> against which this is compared for
+	 * @param other the pattern against which this is compared for
 	 *              onset equality.
-	 * @return true if the onset times of notes in this <code>Pattern</code> match
+	 * @return true if the onset times of notes in this pattern match
 	 * the onset times of notes in other, otherwise returns false
 	 */
 	boolean equalsInOnsets(Pattern other);
