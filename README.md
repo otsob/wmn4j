ffd1f8864b3cf83fe9c13684331d02b1a05fe720
Otso Björklund <otso.bjorklund@gmail.com>
2019-07-09 19:58:39 +0300

    Issue #124: Add version number handling
    * Add class Wmn4j for programmatic access to project information.
    * Generate .properties file for project information for access in
      code.
    * Add package-info for root package of project.
    * Update README to ensure anyone intending to build the project
      that they need to use Gradle.

--- README.md
+++ README.md
@@ -16,6 +16,7 @@ The [API Documentation](https://otsob.github.io/wmn4j) provides a comprehensive 
 ### Building wmn4j
 
 wmn4j uses Gradle and can be built by running Gradle build. With the current configuration the build consists of compilation, unit tests, and static analysis.
+It is recommended to delegate the building of the project to Gradle using the provided Gradle wrapper in the IDE to ensure all dependencies etc. are handled correctly.
 
 ## Contributing
 
---
+++ src/main/java/org/wmn4j/Wmn4j.java
@@ -0,0 +1,56 @@
+package org.wmn4j;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * The properties of the project.
+ */
+public final class Wmn4j {
+
+	private static final Logger LOG = LoggerFactory.getLogger(Wmn4j.class);
+
+	private static final Wmn4j INSTANCE = new Wmn4j();
+
+	private static final String PROPERTIES_FILE_NAME = "wmn4j.properties";
+	private static final String VERSION = "version";
+
+	private final String version;
+
+	private Wmn4j() {
+		version = readVersionFromProperties();
+	}
+
+	private String readVersionFromProperties() {
+		String versionNumberFromProperties = "";
+
+		try (InputStream input = this.getClass().getClassLoader().getResourceAsStream(PROPERTIES_FILE_NAME)) {
+
+			if (input != null) {
+				Properties projectProperties = new Properties();
+				projectProperties.load(input);
+				versionNumberFromProperties = projectProperties.getProperty(VERSION);
+			} else {
+				LOG.warn("Unable to read auto-generated Wmn4j properties file: " + PROPERTIES_FILE_NAME);
+			}
+
+		} catch (IOException exception) {
+			LOG.warn("Reading Wmn4j properties failed with ", exception);
+		}
+
+		return versionNumberFromProperties;
+	}
+
+	/**
+	 * Returns the version number of the project.
+	 *
+	 * @return the version number of the project
+	 */
+	public static String getVersion() {
+		return INSTANCE.version;
+	}
+}
