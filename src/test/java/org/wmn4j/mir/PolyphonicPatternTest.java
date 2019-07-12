ac8e6c2960c95b3add6a868b5dc133ca92b0c399
Otso Björklund <otso.bjorklund@gmail.com>
2019-07-12 20:52:11 +0300

    Issue #6: Add test for polyphonic pattern creation

--- src/test/java/org/wmn4j/mir/PolyphonicPatternTest.java
+++ src/test/java/org/wmn4j/mir/PolyphonicPatternTest.java
@@ -9,11 +9,13 @@ import org.wmn4j.notation.elements.Pitch;
 import org.wmn4j.notation.elements.Rest;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class PolyphonicPatternTest {
 
@@ -34,6 +36,31 @@ public class PolyphonicPatternTest {
 		voices.put(2, voice2);
 
 		return voices;
+	}
+
+	@Test
+	void testCreatingPatternWithInvalidContentsPossible() {
+		List<Durational> nullContents = null;
+		Map<Integer, List<? extends Durational>> nullVoices = null;
+
+		assertThrows(NullPointerException.class, () -> new PolyphonicPattern(nullContents),
+				"Did not throw NullPointerException when trying to create polyphonic pattern with null contents");
+
+		assertThrows(NullPointerException.class, () -> new PolyphonicPattern(nullVoices),
+				"Did not throw NullPointerException when trying to create polyphonic pattern with null voices");
+
+		assertThrows(IllegalArgumentException.class, () -> new PolyphonicPattern(Collections.emptyMap()),
+				"Did not throw IllegalArgumentException when trying to create polyphonic pattern with empty voices");
+
+		List<Note> notes = new ArrayList<>();
+		notes.add(Note.of(Pitch.of(Pitch.Base.B, 0, 3), Durations.QUARTER));
+		notes.add(Note.of(Pitch.of(Pitch.Base.C, 0, 3), Durations.QUARTER));
+
+		Map<Integer, List<? extends Durational>> voices = new HashMap<>();
+		voices.put(1, notes);
+
+		assertThrows(IllegalArgumentException.class, () -> new PolyphonicPattern(voices),
+				"Did not throw IllegalArgumentException when trying to create polyphonic pattern with monophonic contents");
 	}
 
 	@Test
