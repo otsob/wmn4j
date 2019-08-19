5cabebde6d6362e1805c2903e1aab69e7a4cdfa5
Otso Björklund <otso.bjorklund@gmail.com>
2019-08-19 19:02:10 +0300

    Issue #44: Add interface for Pattern discovery results

---
+++ src/main/java/org/wmn4j/mir/discovery/PatternDiscovery.java
@@ -0,0 +1,60 @@
+package org.wmn4j.mir.discovery;
+
+import org.wmn4j.mir.Pattern;
+import org.wmn4j.mir.PatternPosition;
+import org.wmn4j.notation.elements.Score;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+/**
+ * Represents the result of pattern discovery on score.
+ * <p>
+ * Pattern discovery is the process of finding musically important patterns,
+ * such as patterns that are repeated often, from a score.
+ */
+public interface PatternDiscovery {
+
+	/**
+	 * Returns the repeated patterns found by this pattern discovery.
+	 * <p>
+	 * Each returned collection of patterns contains all occurrences of the same or similar
+	 * pattern found by this pattern discovery.
+	 *
+	 * @return the repeated patterns found by this pattern discovery
+	 */
+	default Collection<Collection<Pattern>> getPatterns() {
+		Collection<Collection<PatternPosition>> allPatternPositions = getPatternPositions();
+		Collection<Collection<Pattern>> allPatterns = new ArrayList<>(allPatternPositions.size());
+
+		final Score score = getScore();
+
+		for (Collection<PatternPosition> positions : allPatternPositions) {
+			Collection<Pattern> patterns = new ArrayList<>(positions.size());
+			for (PatternPosition position : positions) {
+				patterns.add(score.getAt(position));
+			}
+
+			allPatterns.add(patterns);
+		}
+
+		return allPatterns;
+	}
+
+	/**
+	 * Returns the positions of the repeated patterns found by this pattern discovery.
+	 * <p>
+	 * Each returned collection of positions contains all positions of occurrences of the same or similar
+	 * pattern found by this pattern discovery.
+	 *
+	 * @return the repeated patterns found by this pattern discovery
+	 */
+	Collection<Collection<PatternPosition>> getPatternPositions();
+
+	/**
+	 * Returns the score to which this pattern discovery applies.
+	 *
+	 * @return the score to which this pattern discovery applies
+	 */
+	Score getScore();
+}
