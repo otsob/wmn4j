a82cb932d1b19066f4f7c2075394472f456a66dd
Otso Björklund <otso.bjorklund@gmail.com>
2019-10-22 19:40:35 +0300

    Issue #45: Implement Chromagrambuilder
    Implement a builder class for chromagrams.

---
+++ src/main/java/org/wmn4j/analysis/harmony/ChromagramBuilder.java
@@ -0,0 +1,115 @@
+/*
+ * Distributed under the MIT license (see LICENSE.txt or https://opensource.org/licenses/MIT).
+ */
+package org.wmn4j.analysis.harmony;
+
+import org.wmn4j.notation.elements.Chord;
+import org.wmn4j.notation.elements.Note;
+import org.wmn4j.notation.elements.PitchClass;
+
+import java.util.EnumMap;
+import java.util.Map;
+import java.util.function.Function;
+
+/**
+ * Builder for {@link Chromagram} instances.
+ * <p>
+ * Instances of this class are not thread-safe.
+ */
+public final class ChromagramBuilder {
+
+	private final Function<Note, Double> weightFunction;
+	private final Map<PitchClass, Double> profile;
+
+	/**
+	 * Returns a weight value for the note that is the double
+	 * value of its duration.
+	 *
+	 * @param note the note for which the weight is returned
+	 * @return a weight value for the note that is the double
+	 * value of its duration
+	 */
+	public static Double durationWeight(Note note) {
+		return note.getDuration().toDouble();
+	}
+
+	/**
+	 * Constructor that creates a builder that adds to the pitch class
+	 * bins based on count of notes or pitch classes. For example, adding a note
+	 * with the pitch class C to builder created with this constructor will
+	 * increment the value of C by 1.0.
+	 */
+	public ChromagramBuilder() {
+		this(note -> 1.0);
+	}
+
+	/**
+	 * Constructor that creates a builder that adds to the pitch class
+	 * bins using the given weight function.
+	 * <p>
+	 * When adding a note to the  builder the value that is added for
+	 * the corresponding pitch class is the value returned by the
+	 * specified weight function. The weight function must produce only
+	 * non-negative values.
+	 *
+	 * @param weightFunction the weight function used for computing the non-negative
+	 *                       weight of a notes's pitch class
+	 */
+	public ChromagramBuilder(Function<Note, Double> weightFunction) {
+		this.weightFunction = weightFunction;
+		this.profile = new EnumMap<>(PitchClass.class);
+		for (PitchClass pc : PitchClass.values()) {
+			profile.put(pc, 0.0);
+		}
+	}
+
+	/**
+	 * Sets the value for the given pitch class.
+	 *
+	 * @param pc    the pitch class for which the value is set
+	 * @param value a non-negative value to set for the pitch class
+	 */
+	public void setValue(PitchClass pc, double value) {
+		if (value < 0.0) {
+			throw new IllegalArgumentException("value must be at least 0.0");
+		}
+
+		this.profile.put(pc, value);
+	}
+
+	/**
+	 * Increments the values for the the pitch classes of the notes in the given chord.
+	 * If a weight function is specified, then the added values are provided by the weight
+	 * function.
+	 *
+	 * @param chord chord for which the pitch classes of the notes are incremented
+	 */
+	public void add(Chord chord) {
+		for (Note note : chord) {
+			this.add(note);
+		}
+	}
+
+	/**
+	 * Increments the value of the pitch class of the note.
+	 * If a weight function is specified, then the added value is provided by the weight
+	 * function.
+	 *
+	 * @param note note for which the pitch class is incremented
+	 */
+	public void add(Note note) {
+		final PitchClass pc = note.getPitch().getPitchClass();
+		double value = profile.get(note.getPitch().getPitchClass());
+		value += weightFunction.apply(note);
+		this.setValue(pc, value);
+	}
+
+	/**
+	 * Returns a chromagram with the values set in this builder.
+	 *
+	 * @return a chromagram with the values set in this builder
+	 */
+	public Chromagram build() {
+		return Chromagram.of(profile);
+	}
+}
---
+++ src/test/java/org/wmn4j/analysis/harmony/ChromagramBuilderTest.java
@@ -0,0 +1,105 @@
+/*
+ * Distributed under the MIT license (see LICENSE.txt or https://opensource.org/licenses/MIT).
+ */
+package org.wmn4j.analysis.harmony;
+
+import org.junit.jupiter.api.Test;
+import org.wmn4j.notation.elements.Chord;
+import org.wmn4j.notation.elements.Durations;
+import org.wmn4j.notation.elements.Note;
+import org.wmn4j.notation.elements.Pitch;
+import org.wmn4j.notation.elements.PitchClass;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class ChromagramBuilderTest {
+	// By how much values are allowed fo differ
+	private static final double TOLERANCE = 0.0000000001;
+
+	private static final Note C_EIGHTH = Note.of(Pitch.of(Pitch.Base.C, 0, 4), Durations.EIGHTH);
+	private static final Note E_EIGHTH = Note.of(Pitch.of(Pitch.Base.E, 0, 4), Durations.EIGHTH);
+	private static final Note G_EIGHTH = Note.of(Pitch.of(Pitch.Base.G, 0, 4), Durations.EIGHTH);
+	private static final Note C_SHARP_SIXTEENTH = Note.of(Pitch.of(Pitch.Base.C, 1, 4), Durations.SIXTEENTH);
+	private static final Note A_FLAT_TRIPLET = Note.of(Pitch.of(Pitch.Base.A, -1, 2), Durations.EIGHTH_TRIPLET);
+	private static final Chord C_TRIAD_EIGHTH = Chord.of(C_EIGHTH, E_EIGHTH, G_EIGHTH);
+
+	@Test
+	void testGivenNegativeValueSetValueThrowsException() {
+		final ChromagramBuilder builder = new ChromagramBuilder();
+		assertThrows(IllegalArgumentException.class, () -> builder.setValue(PitchClass.C, -0.1));
+	}
+
+	@Test
+	void testSetValue() {
+		final ChromagramBuilder builder = new ChromagramBuilder();
+		builder.setValue(PitchClass.C, 1.0);
+		builder.setValue(PitchClass.DSHARP_EFLAT, 2.0);
+		builder.setValue(PitchClass.A, 3.0);
+		builder.setValue(PitchClass.B, 4.0);
+
+		final Chromagram profile = builder.build();
+		assertEquals(1.0, profile.getValue(PitchClass.C), TOLERANCE);
+		assertEquals(0.0, profile.getValue(PitchClass.CSHARP_DFLAT), TOLERANCE);
+		assertEquals(0.0, profile.getValue(PitchClass.D), TOLERANCE);
+		assertEquals(2.0, profile.getValue(PitchClass.DSHARP_EFLAT), TOLERANCE);
+		assertEquals(0.0, profile.getValue(PitchClass.E), TOLERANCE);
+		assertEquals(0.0, profile.getValue(PitchClass.F), TOLERANCE);
+		assertEquals(0.0, profile.getValue(PitchClass.FSHARP_GFLAT), TOLERANCE);
+		assertEquals(0.0, profile.getValue(PitchClass.G), TOLERANCE);
+		assertEquals(0.0, profile.getValue(PitchClass.GSHARP_AFLAT), TOLERANCE);
+		assertEquals(3.0, profile.getValue(PitchClass.A), TOLERANCE);
+		assertEquals(0.0, profile.getValue(PitchClass.ASHARP_BFLAT), TOLERANCE);
+		assertEquals(4.0, profile.getValue(PitchClass.B), TOLERANCE);
+	}
+
+	@Test
+	void testAddWithDefaultWeightFunction() {
+		final ChromagramBuilder builder = new ChromagramBuilder();
+		builder.add(C_EIGHTH);
+		builder.add(E_EIGHTH);
+		builder.add(G_EIGHTH);
+		builder.add(C_SHARP_SIXTEENTH);
+		builder.add(C_TRIAD_EIGHTH);
+		builder.add(A_FLAT_TRIPLET);
+
+		final Chromagram profile = builder.build();
+		assertEquals(2.0, profile.getValue(PitchClass.C), TOLERANCE);
+		assertEquals(1.0, profile.getValue(PitchClass.CSHARP_DFLAT), TOLERANCE);
+		assertEquals(0.0, profile.getValue(PitchClass.D), TOLERANCE);
+		assertEquals(0.0, profile.getValue(PitchClass.DSHARP_EFLAT), TOLERANCE);
+		assertEquals(2.0, profile.getValue(PitchClass.E), TOLERANCE);
+		assertEquals(0.0, profile.getValue(PitchClass.F), TOLERANCE);
+		assertEquals(0.0, profile.getValue(PitchClass.FSHARP_GFLAT), TOLERANCE);
+		assertEquals(2.0, profile.getValue(PitchClass.G), TOLERANCE);
+		assertEquals(1.0, profile.getValue(PitchClass.GSHARP_AFLAT), TOLERANCE);
+		assertEquals(0.0, profile.getValue(PitchClass.A), TOLERANCE);
+		assertEquals(0.0, profile.getValue(PitchClass.ASHARP_BFLAT), TOLERANCE);
+		assertEquals(0.0, profile.getValue(PitchClass.B), TOLERANCE);
+	}
+
+	@Test
+	void testAddWithDurationWeightFunction() {
+		final ChromagramBuilder builder = new ChromagramBuilder(ChromagramBuilder::durationWeight);
+		builder.add(C_EIGHTH);
+		builder.add(E_EIGHTH);
+		builder.add(G_EIGHTH);
+		builder.add(C_SHARP_SIXTEENTH);
+		builder.add(C_TRIAD_EIGHTH);
+		builder.add(A_FLAT_TRIPLET);
+
+		final Chromagram profile = builder.build();
+		assertEquals(0.25, profile.getValue(PitchClass.C), TOLERANCE);
+		assertEquals(1.0 / 16.0, profile.getValue(PitchClass.CSHARP_DFLAT), TOLERANCE);
+		assertEquals(0.0, profile.getValue(PitchClass.D), TOLERANCE);
+		assertEquals(0.0, profile.getValue(PitchClass.DSHARP_EFLAT), TOLERANCE);
+		assertEquals(0.25, profile.getValue(PitchClass.E), TOLERANCE);
+		assertEquals(0.0, profile.getValue(PitchClass.F), TOLERANCE);
+		assertEquals(0.0, profile.getValue(PitchClass.FSHARP_GFLAT), TOLERANCE);
+		assertEquals(0.25, profile.getValue(PitchClass.G), TOLERANCE);
+		assertEquals(1.0 / 12.0, profile.getValue(PitchClass.GSHARP_AFLAT), TOLERANCE);
+		assertEquals(0.0, profile.getValue(PitchClass.A), TOLERANCE);
+		assertEquals(0.0, profile.getValue(PitchClass.ASHARP_BFLAT), TOLERANCE);
+		assertEquals(0.0, profile.getValue(PitchClass.B), TOLERANCE);
+	}
+}
