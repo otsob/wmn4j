239b12073692b86f183f5a2e0aea8fc561c980c1
Otso Björklund <otso.bjorklund@gmail.com>
2020-06-16 19:16:42 +0300

    Add Ornament type for grace notes

--- src/main/java/org/wmn4j/notation/Ornament.java
+++ src/main/java/org/wmn4j/notation/Ornament.java
@@ -1,5 +1,9 @@
 package org.wmn4j.notation;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * Represents an ornament, such as a trill or mordent. This class represents ornaments that are linked to
  * a single note, connected notations are represented using the class {@link Notation}.
@@ -60,7 +64,17 @@ public final class Ornament {
 		/**
 		 * Specifies a turn (gruppetto).
 		 */
-		TURN
+		TURN,
+
+		/**
+		 * Specifies one or more grace notes before the actual note.
+		 */
+		GRACE_NOTES,
+
+		/**
+		 * Specifies one or more grace notes to be played after the actual note.
+		 */
+		SUCCEEDING_GRACE_NOTES,
 	}
 
 	/**
@@ -70,13 +84,36 @@ public final class Ornament {
 	 * @return an ornament of the given type
 	 */
 	public static Ornament of(Type type) {
-		return new Ornament(type);
+		return new Ornament(type, Collections.emptyList());
+	}
+
+	/**
+	 * Returns a grace notes ornament.
+	 *
+	 * @param ornamentalNotes the ornamental notes in the preceding list of grace notes
+	 * @return a grace notes ornament
+	 */
+	public static Ornament graceNotes(List<Ornamental> ornamentalNotes) {
+		return new Ornament(Type.GRACE_NOTES, Collections.unmodifiableList(new ArrayList<>(ornamentalNotes)));
+	}
+
+	/**
+	 * Returns a succeeding grace notes ornament.
+	 *
+	 * @param ornamentalNotes the ornamental notes in the succeeding list of grace notes
+	 * @return a succeeding grace notes ornament
+	 */
+	public static Ornament succeedingGraceNotes(List<Ornamental> ornamentalNotes) {
+		return new Ornament(Type.SUCCEEDING_GRACE_NOTES,
+				Collections.unmodifiableList(new ArrayList<>(ornamentalNotes)));
 	}
 
 	private final Type type;
+	private final List<Ornamental> ornamentalNotes;
 
-	private Ornament(Type type) {
+	private Ornament(Type type, List<Ornamental> ornamentalNotes) {
 		this.type = type;
+		this.ornamentalNotes = ornamentalNotes;
 	}
 
 	/**
@@ -86,5 +123,16 @@ public final class Ornament {
 	 */
 	public Type getType() {
 		return type;
+	}
+
+	/**
+	 * Returns the ornamental notes associated with this ornament.
+	 * Returns empty if there are no ornamental notes, e.g., grace notes
+	 * associated with this ornamental.
+	 *
+	 * @return the ornamental notes associated with this ornament
+	 */
+	public List<Ornamental> getOrnamentalNotes() {
+		return ornamentalNotes;
 	}
 }
--- src/test/java/org/wmn4j/notation/OrnamentTest.java
+++ src/test/java/org/wmn4j/notation/OrnamentTest.java
@@ -2,12 +2,28 @@ package org.wmn4j.notation;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class OrnamentTest {
 	@Test
 	void testGivenOrnamentGetTypeReturnsCorrectType() {
 		assertEquals(Ornament.Type.MORDENT, Ornament.of(Ornament.Type.MORDENT).getType());
 		assertEquals(Ornament.Type.TRILL, Ornament.of(Ornament.Type.TRILL).getType());
+	}
+
+	@Test
+	void testGraceNotes() {
+		final GraceNote graceNoteC = GraceNote
+				.of(Pitch.of(Pitch.Base.C, Pitch.Accidental.NATURAL, 4), Durations.QUARTER, Collections.emptySet(),
+						Collections.emptyList(), Collections.emptyList(), Ornamental.Type.GRACE_NOTE);
+
+		Ornament graceNote = Ornament.graceNotes(Arrays.asList(graceNoteC));
+		assertEquals(Ornament.Type.GRACE_NOTES, graceNote.getType());
+		assertEquals(1, graceNote.getOrnamentalNotes().size());
+		assertTrue(graceNote.getOrnamentalNotes().contains(graceNoteC));
 	}
 }
