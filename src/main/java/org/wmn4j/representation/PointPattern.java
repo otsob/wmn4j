2a22f0fcd03cc8c7cc1617e011ea1497b6936cff
Otso Björklund <otso.bjorklund@gmail.com>
2021-12-29 14:15:48 +0200

    Add PointPattern representation

---
+++ src/main/java/org/wmn4j/representation/PointPattern.java
@@ -0,0 +1,117 @@
+/*
+ * Distributed under the MIT license (see LICENSE.txt or https://opensource.org/licenses/MIT).
+ */
+package org.wmn4j.representation;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+/**
+ * Represents a pattern consisting of pitch event points.
+ * <p>
+ * This class is effectively immutable assuming the constructor is correctly used.
+ */
+public final class PointPattern<T extends Point<T>> implements Iterable<T> {
+
+	private final List<T> points;
+	private final int hash;
+
+	/**
+	 * Constructor that takes ownership of the points passed to this.
+	 * NOTE: No other part of code should use the given list of points after
+	 * a call to this constructor.
+	 *
+	 * @param points the points that the new pattern contains
+	 */
+	public PointPattern(List<T> points) {
+		this.points = points;
+		this.hash = computeHash();
+	}
+
+	/**
+	 * Returns the number of points in this pattern.
+	 *
+	 * @return the number of points in this pattern
+	 */
+	public int size() {
+		return this.points.size();
+	}
+
+	/**
+	 * Returns the point at the given index.
+	 *
+	 * @param index index from which point is returned
+	 * @return the point at the given index
+	 */
+	public T get(int index) {
+		return this.points.get(index);
+	}
+
+	/**
+	 * Returns the vectorized representation of this pattern.
+	 * The vectorized representation consists of the difference
+	 * vectors between consecutive points.
+	 *
+	 * @return the vectorized representation of this pattern
+	 */
+	public PointPattern<T> vectorized() {
+
+		if (size() < 2) {
+			return new PointPattern<T>(Collections.emptyList());
+		}
+
+		final List<T> vecPoints = new ArrayList<>();
+
+		for (int i = 1; i < this.points.size(); ++i) {
+			vecPoints.add(this.points.get(i).subtract(this.points.get(i - 1)));
+		}
+
+		return new PointPattern<>(vecPoints);
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (o == this) {
+			return true;
+		}
+
+		if (!(o instanceof PointPattern)) {
+			return false;
+		}
+
+		final PointPattern other = (PointPattern) o;
+		if (this.size() != other.size()) {
+			return false;
+		}
+
+		return this.points.equals(other.points);
+	}
+
+	private int computeHash() {
+		return Objects.hash(this.points);
+	}
+
+	@Override
+	public int hashCode() {
+		return this.hash;
+	}
+
+	@Override
+	public Iterator<T> iterator() {
+		return points.iterator();
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("{");
+		builder.append(String.join(", ", points.stream().map(T::toString).collect(Collectors.toList())));
+		builder.append("}");
+
+		return builder.toString();
+	}
+}
---
+++ src/test/java/org/wmn4j/representation/PointPatternTest.java
@@ -0,0 +1,78 @@
+/*
+ * Distributed under the MIT license (see LICENSE.txt or https://opensource.org/licenses/MIT).
+ */
+package org.wmn4j.representation;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+public class PointPatternTest {
+
+	private List<Point2D> getTestPoints() {
+		List<Point2D> points = new ArrayList<>();
+
+		points.add(new Point2D(0.33, 50));
+		points.add(new Point2D(5, 64));
+		points.add(new Point2D(27, 12));
+		points.add(new Point2D(39.125, 91));
+		points.add(new Point2D(30003, 3));
+
+		return points;
+	}
+
+	@Test
+	void testPointPatternHasCorrectContents() {
+		List<Point2D> points = getTestPoints();
+		final PointPattern<Point2D> pattern = new PointPattern<>(points);
+
+		assertEquals(5, pattern.size());
+		assertEquals(points.get(0), pattern.get(0));
+		assertEquals(points.get(1), pattern.get(1));
+		assertEquals(points.get(2), pattern.get(2));
+		assertEquals(points.get(3), pattern.get(3));
+		assertEquals(points.get(4), pattern.get(4));
+	}
+
+	@Test
+	void testEqualsAndHashCode() {
+		final PointPattern<Point2D> patternA = new PointPattern<>(getTestPoints());
+		final PointPattern<Point2D> patternB = new PointPattern<>(getTestPoints());
+
+		assertEquals(patternA, patternA);
+		assertEquals(patternA, patternB);
+		assertEquals(patternB, patternA);
+
+		assertEquals(patternA.hashCode(), patternB.hashCode());
+
+		List<Point2D> points = getTestPoints();
+		points.add(1, new Point2D(5, 64));
+
+		final PointPattern<Point2D> patternC = new PointPattern<>(points);
+		assertNotEquals(patternA, patternC);
+	}
+
+	@Test
+	void testVectorizedRepresentation() {
+		List<Point2D> points = getTestPoints();
+		final PointPattern<Point2D> pattern = new PointPattern<>(points);
+		final PointPattern<Point2D> vectorizedPattern = pattern.vectorized();
+
+		assertEquals(4, vectorizedPattern.size());
+
+		assertEquals(points.get(1).subtract(points.get(0)), vectorizedPattern.get(0));
+		assertEquals(points.get(2).subtract(points.get(1)), vectorizedPattern.get(1));
+		assertEquals(points.get(3).subtract(points.get(2)), vectorizedPattern.get(2));
+		assertEquals(points.get(4).subtract(points.get(3)), vectorizedPattern.get(3));
+
+		final PointPattern<Point2D> singletonPattern = new PointPattern<>(
+				Collections.singletonList(new Point2D(0.33, 50)));
+
+		assertEquals(0, singletonPattern.vectorized().size());
+	}
+}
